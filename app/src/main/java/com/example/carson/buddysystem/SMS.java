c155f43e5d470500d7fbf784e212111776ae60eb
jr8699 <jrebok2@gmail.com>
2016-12-03 11:48:06 -0500

    Why is this broken

--- app/src/main/java/com/example/carson/buddysystem/SMS.java
+++ app/src/main/java/com/example/carson/buddysystem/SMS.java
@@ -3,7 +3,7 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.gsm.SmsManager;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +14,25 @@ import android.widget.Toast;
  */
 
 public class SMS extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.buddysystem);
+    }
+
     protected void sendText(String number, String location, String time){
         PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, SMS.class), 0);
         SmsManager sms = SmsManager.getDefault();
         String message = "I will be at: " + location +".\n" +   //Message to send
-                            "I will be back at: " + time ".\n";
+                            "I will be back at: " + time + ".\n";
         sms.sendTextMessage(number, null, message, pi, null);
+
+        //Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        //sendIntent.putExtra("sms_body", message);
+        //sendIntent.setType("vnd.android-dir/mms-sms");
+        //startActivity(sendIntent);
     }
 }
