e9df7e9fb9b1e9b7095ca2113b8fb0d751584d6b
jr8699 <jrebok2@gmail.com>
2016-12-03 10:03:08 -0500

    Updated MainActivity
    getters/setters for information

--- app/src/main/java/com/example/carson/buddysystem/MainActivity.java
+++ app/src/main/java/com/example/carson/buddysystem/MainActivity.java
@@ -5,9 +5,35 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String phoneNumber; //information about where the user is going
+    private String location;
+    private String timeToBeBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Send a text message
+     * Used when a button is pressed for now
+     */
+    protected void onSendTextMessage(){
+
+
+    }
+
+    //Getter and setter for phoneNumber
+    public String getPhoneNumber(){return phoneNumber;}
+    public void setPhoneNumber(String s){this.phoneNumber = s;}
+
+    //Getter and setter for location
+    public String getLocation() {return location;}
+    public void setLocation(String s) {this.location = s;}
+
+    //Getter and setter for timeToBeBack
+    public String getTimeToBeBack() {return timeToBeBack;}
+    public void setTimeToBeBack(String s) {this.timeToBeBack = s;}
+
 }
