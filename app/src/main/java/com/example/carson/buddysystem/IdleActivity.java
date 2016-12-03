aaa9f7c83b31b3597f6b1255ba1b18a35e082a47
jr8699 <jrebok2@gmail.com>
2016-12-03 14:50:25 -0500

    Timer now works

--- app/src/main/java/com/example/carson/buddysystem/IdleActivity.java
+++ app/src/main/java/com/example/carson/buddysystem/IdleActivity.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.math.BigInteger;
+import java.util.Calendar;
 
 /**
  * Activity for when waiting to hear
@@ -30,7 +32,10 @@ public class IdleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_idle);
 
         _t = new Timer();
-        _t.schedule(new WaitTask(), 10000); //in ms? //add real time in here when ready
+        int taskTime = stringToMS(alertObject.getTimeToBeBack());
+        System.out.println("IODEBUG " + taskTime + "  " + alertObject.getTimeToBeBack());
+        _t.schedule(new WaitTask(), taskTime); //in ms? //add real time in here when ready
+        //~11/12 days would be the maximum with this or an overflow will occur
 
         /**
          Button listener to cancel the text.
@@ -39,8 +44,8 @@ public class IdleActivity extends AppCompatActivity {
         reportBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onSendTextMessage();
-                //_t.cancel(); //cancel the task since the user clicked the button
+                _t.cancel(); //cancel the task since the user clicked the button
+                IdleActivity.super.onBackPressed();
             }
         });
     }
@@ -48,7 +53,9 @@ public class IdleActivity extends AppCompatActivity {
     class WaitTask extends TimerTask{
 
         public void run (){//runs if the user does not respond to the button
-            //ask if done here
+            onSendTextMessage();
+            //could have a better message
+            IdleActivity.super.onBackPressed();
             _t.cancel(); //end the task
         }
     }
@@ -69,6 +76,52 @@ public class IdleActivity extends AppCompatActivity {
         //sendIntent.putExtra("sms_body", message);
         //sendIntent.setType("vnd.android-dir/mms-sms");
         //startActivity(sendIntent);
+    }
+
+    /**
+     * Convert a time string into milliseconds
+     * @param s
+     * @return
+     */
+    protected int stringToMS(String s){
+        int i = 0;
+        String sub = "";
+        int hours = -1;
+        int minutes = -1;
+
+        //current time
+        Calendar c = Calendar.getInstance();
+        int currentHour = c.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = c.get(Calendar.MINUTE);
+
+        while(i<s.length()){
+            if(s.charAt(i) != ':'){ //check to see if we want to split
+                sub = sub + s.charAt(i);
+            }else{
+                if(hours == -1) { //parse the hours
+                    hours = Integer.parseInt(sub);
+                    sub = ""; //reset the substring
+                }
+                else //parse the minutes
+                    minutes = Integer.parseInt(sub);
+            }
+            i++;
+        }
+
+        //only able to go out 24hours with the current implementation
+        if(hours < currentHour) //error handling, set to next day if hour is before
+                hours = hours + 24;
+
+        //convert it all into milliseconds
+        int end = 0;
+        if(hours > 0) end = 1000*60*60*hours;
+        if(minutes > 0) end = end + 1000*60*minutes;
+
+        int current = 0;
+        if(currentHour > 0) current = 1000*60*60*hours;
+        if(currentMinute > 0) current = current + 1000*60*minutes;
+
+        return Math.abs(current - end);
     }
 }
 
