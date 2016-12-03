7e5688965d08939b3b64bd68d2f76fba12e952e6
carsnwd <carsnwd@gmail.com>
2016-12-03 09:32:41 -0500

    Make sure everything is correct

--- app/src/main/java/com/example/carson/buddysystem/MainActivity.java
+++ app/src/main/java/com/example/carson/buddysystem/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    Testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
