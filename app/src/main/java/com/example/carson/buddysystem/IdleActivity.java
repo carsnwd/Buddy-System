51266f2e779fcc83d15d996c849b8352e8de9bad
jr8699 <jrebok2@gmail.com>
2016-12-03 13:17:00 -0500

    Time ready to go

--- app/src/main/java/com/example/carson/buddysystem/IdleActivity.java
+++ app/src/main/java/com/example/carson/buddysystem/IdleActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Activity for when waiting to hear
@@ -13,11 +15,16 @@ import android.widget.EditText;
  * will cancel the not reported back text if so.
  */
 public class IdleActivity extends AppCompatActivity {
+    public Timer _t; //timer for the task
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idle);
+
+        _t = new Timer();
+        _t.schedule(new WaitTask(), 10000); //in ms? //add real time in here when ready
 
         /**
          Button listener to cancel the text.
@@ -26,8 +33,17 @@ public class IdleActivity extends AppCompatActivity {
         reportBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                _t.cancel(); //cancel the task since the user clicked the button
             }
         });
     }
+
+    class WaitTask extends TimerTask{
+
+        public void run (){//runs if the user does not respond to the button
+            //ask if done here
+            _t.cancel(); //end the task
+        }
+    }
 }
+
